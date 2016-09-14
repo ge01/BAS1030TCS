1575107b8d00ac1d88ade78bb8f9bbfc4f29bb48
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-09-13 20:31:31 -0500

    rev2 it works with 3 test strings

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ function titleCase(str) {
   var capArray = [];
   // Turn string (str) into an array - split(' ')
   var arrayStr = str.split(' ');
-  console.log(arrayStr);
+  //console.log(arrayStr);
 
   // Loop throug each word in the array
   for (var i = 0; i < arrayStr.length; i++){
-    console.log(arrayStr[i].substring(0,1).toUpperCase() + arrayStr[i].slice(1).toLowerCase());
+    //console.log(arrayStr[i].substring(0,1).toUpperCase() + arrayStr[i].slice(1).toLowerCase());
     capArray.push(arrayStr[i].substring(0,1).toUpperCase() + arrayStr[i].slice(1).toLowerCase());
 
   }
-  console.log(capArray);
+  //console.log(capArray);
   str = capArray.join(' ');
 
   return str;
 }
 
 console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("sHoRt AnD sToUt"));
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
+
 
 //var myArray = ["I'm", "a", "little", "tea", "pot"];
 
